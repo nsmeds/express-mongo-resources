137ce1bccc3797acf6e7bedd9bfe1f86d4fa0179
nsmeds <niilosmeds@gmail.com>
2016-11-02 15:19:41 -0700

    Basic routes and models written; things seem to be working in Postman; no tests written yet

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./lib/app');
+const port = process.env.PORT || 3000;
+const http = require('http');
+const server = http.createServer(app);
+require('./lib/setup-mongoose');
+
+server.listen(port, () => {
+  console.log('server listening on port ', port);
+});
